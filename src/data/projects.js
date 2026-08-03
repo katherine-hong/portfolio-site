f8c2f1d471cadbeab3ccf75edb1593dac7b7af31
Katherine Hong <kkhong@terpmail.umd.edu>
2026-08-02 23:54:35 -0400

    Add basic project page + card layouts

---
+++ src/data/projects.js
@@ -0,0 +1,38 @@
+export const projects = [
+  {
+    slug: 'project-one',
+    title: 'Project One',
+    description: 'A one-line description of what this project does.',
+    image: '/images/project-one.png',
+  },
+  {
+    slug: 'project-two',
+    title: 'Project Two',
+    description: 'A one-line description of what this project does.',
+    image: '/images/project-two.png',
+  },
+  {
+    slug: 'project-three',
+    title: 'Project Three',
+    description: 'A one-line description of what this project does.',
+    image: '/images/project-three.png',
+  },
+  {
+    slug: 'project-four',
+    title: 'Project Four',
+    description: 'A one-line description of what this project does.',
+    image: '/images/project-four.png',
+  },
+  {
+    slug: 'project-five',
+    title: 'Project Five',
+    description: 'A one-line description of what this project does.',
+    image: '/images/project-five.png',
+  },
+  {
+    slug: 'project-six',
+    title: 'Project Six',
+    description: 'A one-line description of what this project does.',
+    image: '/images/project-six.png',
+  }
+];
